4895adbbb35ea117346ce027313f881841b077bd
donovanduyho <donovanduyho@gmail.com>
2024-09-27 22:12:22 -0400

    modify ALLOWED_HOSTS in settings.py

--- server/server/settings.py
+++ server/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%%+9vv0uez779+!_1n3^k8@(much$!@0u0wyn*_q1oz=%ub)5^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
