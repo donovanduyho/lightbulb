296ece452d1713963c5e1b6bf680608186c01c5e
donovanduyho <donovanduyho@gmail.com>
2024-09-28 14:48:05 -0400

    test

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Input } from '@/components/ui/input';
 
 export default function Login() {
-  return <div>page</div>;
+  return (
+    <div className="w-1/2">
+      <Input></Input>
+      <Input></Input>
+    </div>
+  );
 }
