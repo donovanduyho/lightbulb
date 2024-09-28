eae72893f81fca99ceca43504b968c0dcfb2c3e8
ajm1312 <andrew.mitchell11@yahoo.com>
2024-09-28 13:54:56 -0400

    changed server to express

---
+++ server/helpers/passwordcheck.js
@@ -0,0 +1,27 @@
+const bcrypt = require('bcrypt');
+
+class passwordCheck {
+    static hashPassword = (password) => {
+        return new Promise ((resolve, reject) => {
+            bcrypt.genSalt(12, (err, salt) => {
+                if (err) {
+                    reject(err);
+                }
+                bcrypt.hash(password, salt, (err, hash) => {
+                    if (err){
+                        reject(err);
+                    }
+                    resolve(hash);
+                });
+            })
+        })
+    }
+
+
+    static comparePassword = (password, hashed) => {
+        return bcrypt.compare(password, hashed)
+    }
+}
+
+
+module.exports = passwordCheck
