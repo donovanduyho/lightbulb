65397b8a903930d96dadab765432258d17eae189
donovanduyho <donovanduyho@gmail.com>
2024-09-29 05:45:40 -0400

    edit signup form

--- client/app/register/_components/signup-form.tsx
+++ client/app/register/_components/signup-form.tsx
@@ -8,6 +8,7 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import { Switch } from '@/components/ui/switch';
 import { Button } from '@/components/ui/button';
 import { FormEvent } from 'react';
 
@@ -20,11 +21,13 @@ export default function SignupForm() {
     const password = formData.get('password');
     const first_name = formData.get('fname');
     const last_name = formData.get('lname');
+    const is_admin = formData.get('admin');
 
     console.log(username);
     console.log(password);
     console.log(first_name);
     console.log(last_name);
+    console.log(is_admin);
 
     // Post to endpoint -> Redirect to dashboard
   };
@@ -35,18 +38,22 @@ export default function SignupForm() {
         <CardTitle>Create an account</CardTitle>
       </CardHeader>
       <form onSubmit={handleSubmit}>
-        <CardContent className="flex flex-col gap-3">
-          <p>Username</p>
+        <CardContent className="flex flex-col gap-2">
+          <label>Username</label>
           <Input type="username" name="username" required></Input>
-          <p>Password</p>
+          <label>Password</label>
           <Input type="password" name="password" required></Input>
-          <p>First Name</p>
+          <label>First Name</label>
           <Input type="fname" name="fname" required></Input>
-          <p>Last Name</p>
+          <label>Last Name</label>
           <Input type="lname" name="lname" required></Input>
+          <div className="flex flex-row gap-3 items-center mt-3">
+            <label>Admin</label>
+            <Switch name="admin" />
+          </div>
         </CardContent>
         <CardFooter className="flex justify-between">
-          <Button>Register</Button>
+          <Button className="rounded-full">Register</Button>
         </CardFooter>
       </form>
     </Card>
