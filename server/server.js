1beb69b0c46cb872dfba6271c14b01fe9b6846fc
donovanduyho <donovanduyho@gmail.com>
2024-09-29 06:30:04 -0400

    test

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
-const express = require("express");
-const router = express.Router()
-const dotenv = require("dotenv")
+const express = require('express');
+const router = express.Router();
+const dotenv = require('dotenv');
+const cors = require('cors');
 
-const app = express()
+const app = express();
+app.use(cors());
 dotenv.config();
 
 app.use(express.json());
-app.use('/', require("../server/routes/routes"))
+app.use('/', require('../server/routes/routes'));
 
 const port = 8000;
-app.listen(port, () => console.log(`server running on port ${port}`))
+app.listen(port, () => console.log(`server running on port ${port}`));
