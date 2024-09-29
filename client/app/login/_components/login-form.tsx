f6063308318c5f61b6926fc0a2278e2f4aa5f252
donovanduyho <donovanduyho@gmail.com>
2024-09-29 06:52:14 -0400

    modify login form

--- client/app/login/_components/login-form.tsx
+++ client/app/login/_components/login-form.tsx
@@ -1,7 +1,7 @@
-"use client";
-import React from "react";
-import { Input } from "@/components/ui/input";
-import { useRouter } from "next/navigation";
+'use client';
+import React from 'react';
+import { Input } from '@/components/ui/input';
+import { useRouter } from 'next/navigation';
 
 import {
   Card,
@@ -9,48 +9,44 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { FormEvent, useState } from "react";
+} from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { FormEvent } from 'react';
 
-export default async function LoginForm() {
+export default function LoginForm() {
   const { push } = useRouter();
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const formData = new FormData(e.currentTarget);
-    const username = formData.get("username");
-    const password = formData.get("password");
+    const UserName = formData.get('username');
+    const Password = formData.get('password');
 
     const input = {
-      username,
-      password,
+      UserName,
+      Password,
     };
 
-    console.log(username);
-    console.log(password);
+    console.log(UserName);
+    console.log(Password);
 
     try {
-      const response = await fetch(
-        "http://localhost:8000/user/registerStudent",
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(input),
-        }
-      );
+      const response = await fetch('http://localhost:8000/login/loginUser', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(input),
+      });
 
       if (response.ok) {
         const result = await response.json();
         console.log(result);
-        push("/dashboard");
+        push('/dashboard');
       } else {
-        console.log("Failed to register.");
+        console.log('Failed to log in.');
       }
     } catch (error) {
-      console.log("Error:", error);
+      console.log('Error:', error);
     }
-    // Post to endpoint -> Redirect to dashboard
   };
 
   return (
